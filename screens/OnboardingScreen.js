d82ced117fa07a2f39b169f844df9117a8fdbb01
dotienanh77 <anhdt.ute@gmail.com>
2021-06-30 11:43:30 +0700

    On boarding

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,12 +1,34 @@
 /* eslint-disable no-alert */
 import React from 'react';
-import {Text, View, StyleSheet, Button} from 'react-native';
+import {Text, View, StyleSheet, Image, Button} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
 const OnboardingScreen = ({navigation}) => {
   return (
-    <View style={styles.container}>
-      <Text>Onboarding Screen</Text>
-      <Button title="Click here" onPress={() => navigation.navigate('Login')} />
-    </View>
+    <Onboarding
+      onDone={() => navigation.navigate('Login')}
+      onSkip={() => navigation.navigate('Login')}
+      pages={[
+        {
+          backgroundColor: '#a6d4e0',
+          image: <Image source={require('../assets/onboarding-img1.png')} />,
+          title: 'Connect to the World',
+          subtitle: 'A new way to connect with the world!',
+        },
+        {
+          backgroundColor: '#fdeb93',
+          image: <Image source={require('../assets/onboarding-img2.png')} />,
+          title: 'Onboarding 2',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#e9bcbe',
+          image: <Image source={require('../assets/onboarding-img3.png')} />,
+          title: 'Onboarding 3',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+      ]}
+    />
   );
 };
 export default OnboardingScreen;
