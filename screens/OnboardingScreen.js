8ffda1620eb7c0dbac7d37390f3c8e5c6744513a
dotienanh77 <anhdt.ute@gmail.com>
2021-06-30 15:39:04 +0700

    AsyncStorage On boarding screen

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -1,31 +1,75 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable no-alert */
 import React from 'react';
-import {Text, View, StyleSheet, Image, Button} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  Button,
+} from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
+const Dots = ({selected}) => {
+  let backgroundColor;
 
+  backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+  return (
+    <View
+      style={{
+        width: 6,
+        height: 6,
+        marginHorizontal: 3,
+        backgroundColor,
+      }}
+    />
+  );
+};
+const Skip = ({...props}) => (
+  <TouchableOpacity style={{marginHorizontal: 10}} {...props}>
+    <Text style={{fontSize: 16}}>Skip</Text>
+  </TouchableOpacity>
+);
+
+const Next = ({...props}) => (
+  <TouchableOpacity style={{marginHorizontal: 10}} {...props}>
+    <Text style={{fontSize: 16}}>Next</Text>
+  </TouchableOpacity>
+);
+
+const Done = ({...props}) => (
+  <TouchableOpacity style={{marginHorizontal: 10}} {...props}>
+    <Text style={{fontSize: 16}}>Done</Text>
+  </TouchableOpacity>
+);
 const OnboardingScreen = ({navigation}) => {
   return (
     <Onboarding
+      SkipButtonComponent={Skip}
+      NextButtonComponent={Next}
+      DoneButtonComponent={Done}
+      DotComponent={Dots}
       onDone={() => navigation.navigate('Login')}
-      onSkip={() => navigation.navigate('Login')}
+      onSkip={() => navigation.replace('Login')}
       pages={[
         {
-          backgroundColor: '#a6d4e0',
+          backgroundColor: '#a6e4d0',
           image: <Image source={require('../assets/onboarding-img1.png')} />,
           title: 'Connect to the World',
-          subtitle: 'A new way to connect with the world!',
+          subtitle: 'A New Way To Connect With The World',
         },
         {
           backgroundColor: '#fdeb93',
           image: <Image source={require('../assets/onboarding-img2.png')} />,
-          title: 'Onboarding 2',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          title: 'Share Your Favorites',
+          subtitle: 'Share Your Thoughts With Similar Kind of People',
         },
         {
           backgroundColor: '#e9bcbe',
           image: <Image source={require('../assets/onboarding-img3.png')} />,
-          title: 'Onboarding 3',
-          subtitle: 'Done with React Native Onboarding Swiper',
+          title: 'Become The Star',
+          subtitle: 'Let The Spot Light Capture You',
         },
       ]}
     />
