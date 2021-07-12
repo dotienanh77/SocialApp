d187820aa8bb031cbc99dec22dec1175d7d649bd
dotienanh77 <anhdt.ute@gmail.com>
2021-07-12 15:03:20 +0700

    postscreen

--- screens/AddPostScreen.js
+++ screens/AddPostScreen.js
@@ -1,36 +1,64 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Platform} from 'react-native';
+import ImagePicker from 'react-native-image-crop-picker';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {InputWrapper, InputField} from '../styles/AddPost';
+import {
+  InputWrapper,
+  InputField,
+  AddImage,
+  StatusWrapper,
+  SubmitBtn,
+  SubmitBtnText,
+} from '../styles/AddPost';
+
 const AddPostScreen = () => {
+  const [image, setImage] = useState(null);
+
+  const takePhotoFromCamera = () => {
+    ImagePicker.openCamera({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      const imageUri = Platform.OS === 'ios' ? image.sourceURL : image.path;
+      setImage(imageUri);
+    });
+  };
+  const choosePhotoFromLibrary = () => {
+    ImagePicker.openPicker({
+      width: 1200,
+      height: 780,
+      cropping: true,
+    }).then(image => {
+      console.log(image);
+      const imageUri = Platform.OS === 'ios' ? image.sourceURL : image.path;
+      setImage(imageUri);
+    });
+  };
   return (
     <View style={styles.container}>
       <InputWrapper>
+        {image != null ? <AddImage source={{uri: image}} /> : null}
         <InputField
           placeholder="What's do you mind?"
           multiline={true}
           numberOfLines={4}
         />
       </InputWrapper>
-      <ActionButton buttonColor="rgba(231,76,60,1)">
+      <ActionButton buttonColor="#2e64e5">
         <ActionButton.Item
           buttonColor="#9b59b6"
-          title="New Task"
-          onPress={() => console.log('notes tapped!')}>
-          <Icon name="md-create" style={styles.actionButtonIcon} />
+          title="Take Photo"
+          onPress={takePhotoFromCamera}>
+          <Icon name="camera-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
         <ActionButton.Item
           buttonColor="#3498db"
-          title="Notifications"
-          onPress={() => {}}>
-          <Icon name="md-notifications-off" style={styles.actionButtonIcon} />
-        </ActionButton.Item>
-        <ActionButton.Item
-          buttonColor="#1abc9c"
-          title="All Tasks"
-          onPress={() => {}}>
-          <Icon name="md-done-all" style={styles.actionButtonIcon} />
+          title="Choose Photo"
+          onPress={choosePhotoFromLibrary}>
+          <Icon name="md-image-outline" style={styles.actionButtonIcon} />
         </ActionButton.Item>
       </ActionButton>
     </View>
