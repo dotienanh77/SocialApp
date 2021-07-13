2a4168226d4626332d5e0fe7a1231562280c3d59
dotienanh77 <anhdt.ute@gmail.com>
2021-07-13 11:29:38 +0700

    fetchPosts orget documents from Firestore Database

--- components/PostCard.js
+++ components/PostCard.js
@@ -38,15 +38,15 @@ const PostCard = ({item}) => {
   return (
     <Card>
       <UserInfo>
-        <UserImg source={item.userImg} />
+        <UserImg source={{uri: item.userImg}} />
         <UserInfoText>
           <UserName>{item.userName}</UserName>
-          <PostTime>{item.postTime}</PostTime>
+          <PostTime>{item.postTime.toString()}</PostTime>
         </UserInfoText>
       </UserInfo>
       <PostText>{item.post}</PostText>
-      {item.postImg !== 'none' ? (
-        <PostImg source={item.postImg} />
+      {item.postImg !== null ? (
+        <PostImg source={{uri: item.postImg}} />
       ) : (
         <Divider />
       )}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import {FlatList} from 'react-native';
-
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  ScrollView,
+  Text,
+  StyleSheet,
+  FlatList,
+  SafeAreaView,
+  Alert,
+} from 'react-native';
+// import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
+import storage from '@react-native-firebase/storage';
+import firestore from '@react-native-firebase/firestore';
 import PostCard from '../components/PostCard';
 import {Container} from '../styles/FeedStyles';
+// import {cos} from 'react-native-reanimated';
+// import PostCard from '../components/PostCard';
 const Posts = [
   {
     id: '1',
@@ -62,10 +75,52 @@ const Posts = [
 ];
 
 const HomeScreen = () => {
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const list = [];
+        await firestore()
+          .collection('posts')
+          .get()
+          .then(querySnapshot => {
+            // console.log('Total Posts: ', querySnapshot.size);
+            querySnapshot.forEach(doc => {
+              const {post, userId, postImg, postTime, likes, comments} =
+                doc.data();
+              list.push({
+                id: doc.id,
+                userId,
+                userName: 'Test name',
+                userImg:
+                  'https://firebasestorage.googleapis.com:443/v0/b/rn-social-app-3f8fb.appspot.com/o/photos%2FIMG_00021626147218325.JPG?alt=media&token=38a1da53-1121-4ca5-b68e-ea0e6d219ff5',
+                postTime: postTime,
+                post,
+                postImg,
+                liked: false,
+                likes,
+                comments,
+              });
+            });
+          });
+
+        setPosts(list);
+        if (loading) {
+          setLoading(false);
+        }
+        console.log('Post: ', list);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <Container>
       <FlatList
-        data={Posts}
+        data={posts}
         renderItem={({item}) => <PostCard item={item} />}
         keyExtractor={item => item.id}
         showsVerticalScrollIndicator={false}
