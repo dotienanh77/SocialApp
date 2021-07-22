91686c788affc1ad89f186015b64659afc471541
dotienanh77 <anhdt.ute@gmail.com>
2021-07-22 17:52:08 +0700

    create chat screen UI

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, {useEffect, useState, useCallback} from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
-
+import {GiftedChat, Bubble} from 'react-native-gifted-chat';
 const ChatScreen = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Hello world',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    );
+  }, []);
+
+  const renderBubble = props => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#2e64e5',
+          },
+        }}
+        textStyle={{
+          right: {
+            color: '#fff',
+          },
+        }}
+      />
+    );
+  };
   return (
-    <View style={styles.container}>
-      <Text>Chat Screen</Text>
-      <Button title="Click me" onPress={() => {}} />
-    </View>
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+      renderBubble={renderBubble}
+    />
   );
 };
 
